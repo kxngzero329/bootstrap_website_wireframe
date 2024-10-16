d2eced93dc65ff268855235749f9b52186bf6320
SMITH MOGAMAT SHUHAIR <126153069+kxngzero329@users.noreply.github.com>
2024-10-17 00:14:22 +0200

    Pure Bootstrap 5 Static Page

---
+++ bootstrap wireframe/script.js
@@ -0,0 +1,9 @@
+// Add a smooth scroll spy feature for highlighting the active nav link
+
+document.addEventListener('DOMContentLoaded', function () {
+    const scrollSpy = new bootstrap.ScrollSpy(document.body, {
+      target: '#navbar',
+      offset: 100
+    });
+  });
+  
